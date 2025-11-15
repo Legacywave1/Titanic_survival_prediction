55fc3b055e75cb6507a55250419071b639391890
Anku Cyprian <cypriananku121@gmail.com>
2025-11-15 13:40:40 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+hypen_e_dot = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    try:
+        with open(file_path, 'r') as file:
+            requirements = file.readlines()
+            requirements = [i.replace('\n', '') for i in requirements]
+            requirements = [req for req in requirements if req and not req.startswith('#')]
+            if hypen_e_dot in requirements:
+                requirements.remove(hypen_e_dot)
+    except FileNotFoundError:
+        print(f'Warning: {file_path} not Found')
+    return requirements
+
+setup(
+    name = 'Titanic-prediction',
+    version = '0.0.1',
+    author = 'Cyprian',
+    author_email = 'cypriananku121@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+    )
---
+++ src/exception.py
@@ -0,0 +1,15 @@
+import sys
+
+def error_message_detail(error, error_details):
+    _,_,exc_tb = error_details.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = f'Error occured in python script {file_name} at line [{exc_tb.tb_lineno}] error message [{str(error)}]'
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message:str, error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_details)
+
+        def __str__(self):
+            return self.error_message
