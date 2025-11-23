64617a8f7140acd106f3c76821608261faf63e3a
Cyprian Anku <105254642+Legacywave1@users.noreply.github.com>
2025-11-23 05:28:30 +0000

    Bump version from 0.0.5 to 0.0.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_requirements(file_path: str) -> List[str]:
 
 setup(
     name = 'Titanic-prediction',
-    version = '0.0.5',
+    version = '0.0.6',
     author = 'Cyprian',
     author_email = 'cypriananku121@gmail.com',
     packages = find_packages(),
